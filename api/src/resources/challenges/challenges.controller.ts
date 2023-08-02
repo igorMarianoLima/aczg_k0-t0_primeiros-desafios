f074990a36650348c3af5ccf3855571fb6a2d6aa
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-08-02 15:00:34 -0300

    feat-api: Feat - Add '/challenges/multiples-of' endpoint in 'Challenges' controller

--- api/src/resources/challenges/challenges.controller.ts
+++ api/src/resources/challenges/challenges.controller.ts
@@ -1,45 +1,17 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Body } from '@nestjs/common';
 import { ChallengesService } from './challenges.service';
-import { CreateChallengeDto } from './dto/create-challenge.dto';
-import { UpdateChallengeDto } from './dto/update-challenge.dto';
+
+import { GetMultiplesNumbersDto } from './dto/get-multiples-numbers.dto';
 
 @Controller('challenges')
 export class ChallengesController {
   constructor(private readonly challengesService: ChallengesService) {}
 
-  @Post()
-  create(@Body() createChallengeDto: CreateChallengeDto) {
-    return this.challengesService.create(createChallengeDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.challengesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.challengesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateChallengeDto: UpdateChallengeDto,
-  ) {
-    return this.challengesService.update(+id, updateChallengeDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.challengesService.remove(+id);
+  @Get('multiples-of')
+  solveMultiplesNumber(@Body() body: GetMultiplesNumbersDto) {
+    return this.challengesService.solveMultiplesNumbers(
+      body.multiplesOf,
+      body.limit,
+    );
   }
 }
