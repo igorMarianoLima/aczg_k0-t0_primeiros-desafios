7f61d9a6885c2faa188da02faa63f8fc9aa60563
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-08-03 18:01:20 -0300

    feat-site: Feat - Add routes to '/challenges' page

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'challenges',
+    loadChildren: () => import('./pages/challenges/challenges.module').then(m => m.ChallengesModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'challenges'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- site/src/app/pages/challenges/challenges-routing.module.ts
+++ site/src/app/pages/challenges/challenges-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChallengesComponent } from './challenges.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ChallengesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
