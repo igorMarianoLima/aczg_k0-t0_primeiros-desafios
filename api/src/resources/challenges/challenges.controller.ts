65796c6233c3c9cf63cd5d74c3d37c0731ecd90b
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-08-03 15:49:45 -0300

    feat-api: Feat - Add '/challenges/fibonacci' endpoint in 'Challenges' controller

--- api/src/resources/challenges/challenges.controller.ts
+++ api/src/resources/challenges/challenges.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Body } from '@nestjs/common';
 import { ChallengesService } from './challenges.service';
 
 import { GetMultiplesNumbersDto } from './dto/get-multiples-numbers.dto';
+import { GetFibonacciPairsSumDto } from './dto/get-fibonacci-pairs-sum.dto';
 
 @Controller('challenges')
 export class ChallengesController {
@@ -13,5 +14,10 @@ export class ChallengesController {
       body.multiplesOf,
       body.limit,
     );
+  }
+
+  @Get('fibonacci')
+  solveFibonacciPairsTermsSum(@Body() body: GetFibonacciPairsSumDto) {
+    return this.challengesService.solveFibonacci(body.limit);
   }
 }
