3a5eb152706e3c7c13304dd8e77ad5c3bd96e885
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-08-03 18:47:27 -0300

    feat-cors: Feat - Enable CORS in API

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,12 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
+
   await app.listen(3000);
 }
 bootstrap();
