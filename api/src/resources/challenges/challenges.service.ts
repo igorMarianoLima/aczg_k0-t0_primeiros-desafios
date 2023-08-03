3631e954f59413aa6c66f2a33447e885a6850dd3
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-08-03 15:48:05 -0300

    feat-api: Feat - Create 'solveFibonacci()' method in 'FibonacciNumbersChallenge' class

--- api/src/resources/challenges/challenges.service.ts
+++ api/src/resources/challenges/challenges.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 import { MultiplesNumbersChallenge } from './entities/multiplesNumbersChallenge.entity';
 import { SolvedChallengeDto } from './dto/solved-challenge.dto';
+import { FibonacciNumbersChallenge } from './entities/fibonacciNumbersChallenge.entity';
 
 @Injectable()
 export class ChallengesService {
@@ -14,6 +15,16 @@ export class ChallengesService {
     );
 
     challenge.solveChallenge(multiplesOf, limit);
+
+    return challenge;
+  }
+
+  solveFibonacci(fibonacciLimit: number): SolvedChallengeDto {
+    const challenge = new FibonacciNumbersChallenge(
+      `Soma dos termos pares da sequência Fibonacci`,
+    );
+
+    challenge.solveChallenge(fibonacciLimit);
 
     return challenge;
   }
