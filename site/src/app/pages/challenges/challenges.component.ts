82b1af25a19cf91756065fe040019253fe1d2647
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-08-03 19:17:05 -0300

    feat-site: Feat - Create 'Challenges' page attributes and methods

--- site/src/app/pages/challenges/challenges.component.ts
+++ site/src/app/pages/challenges/challenges.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
 
+import { ChallengesService } from 'src/app/shared/services/challenges/challenges.service';
+
 @Component({
   selector: 'app-challenges',
   templateUrl: './challenges.component.html',
   styleUrls: ['./challenges.component.scss']
 })
 export class ChallengesComponent {
+  multiplesOf = [3, 5]
+  multiplesBelow = 1000;
+  multiples = null;
 
+  fibonacciLimit = 4000000;
+  fibonacciPairsSum = null;
+
+  numberToFactorate = 600851475143;
+  largestPrime = null;
+
+  constructor(
+    private challengesService: ChallengesService
+  ) {}
+
+  solveMultipleNumbers() {
+    this.challengesService.solveChallenge(
+      'multiples-of',
+      {
+        multiplesOf: this.multiplesOf.map(number => Number(number)),
+        limit: Number(this.multiplesBelow - 1)
+      }
+    ).subscribe({
+      next: d => this.multiples = d.answer
+    })
+  }
+
+  solveFibonacci() {
+    this.challengesService.solveChallenge(
+      'fibonacci',
+      {
+        limit: Number(this.fibonacciLimit)
+      }
+    ).subscribe({
+      next: d => this.fibonacciPairsSum = d.answer
+    })
+  }
+
+  solvePrimeFactors() {
+    this.challengesService.solveChallenge(
+      'prime-factors',
+      {
+        number: Number(this.numberToFactorate)
+      }
+    ).subscribe({
+      next: d => this.largestPrime = d.answer
+    })
+  }
 }
