0f505b219a80c1f23beb4eccfcd8f7e952a484b7
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-08-03 18:51:44 -0300

    feat-site: Feat - Create 'solveChallenge()' method in 'ChallengesService'

--- site/src/app/shared/services/challenges/challenges.service.ts
+++ site/src/app/shared/services/challenges/challenges.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChallengesService {
+  private readonly apiBaseUrl = environment.API_URL_BASE;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  solveChallenge(challengeEndpoint: string, body: any) {
+    return this.http.post(`${this.apiBaseUrl}/challenges/${challengeEndpoint}`, body)
+  }
 }
