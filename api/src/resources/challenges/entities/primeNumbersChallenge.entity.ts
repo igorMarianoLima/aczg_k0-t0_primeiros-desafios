b1c31f0093d368183d4bb1233d5fb0ae77402c48
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-08-03 17:02:50 -0300

    feat-api: Feat - Create 'PrimeNumbersChallenge' entity class

---
+++ api/src/resources/challenges/entities/primeNumbersChallenge.entity.ts
@@ -0,0 +1,41 @@
+import { Challenge } from './challenge.entity';
+
+export class PrimeNumbersChallenge extends Challenge {
+  solveChallenge(number: number) {
+    const primeFactors = [];
+
+    const factoreByPrime = (number: number) => {
+      for (let factor = 2; factor <= number; factor++) {
+        const firstPrimeFactors = [2, 3, 5, 7];
+
+        if (factor === number) {
+          primeFactors.push(factor);
+          break;
+        }
+
+        if (firstPrimeFactors.some((factor) => number % factor === 0)) {
+          const prime = firstPrimeFactors.find(
+            (factor) => number % factor === 0,
+          );
+
+          primeFactors.push(prime);
+          factoreByPrime(number / prime);
+
+          return;
+        }
+
+        if (number % factor === 0) {
+          primeFactors.push(factor);
+          factoreByPrime(number / factor);
+
+          break;
+        }
+      }
+    };
+
+    factoreByPrime(number);
+    this.answer = primeFactors.join(', ');
+
+    return this.answer;
+  }
+}
